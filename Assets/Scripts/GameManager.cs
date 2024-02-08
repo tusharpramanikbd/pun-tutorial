29773929eb1b029b51461104d808066d68725a5e
Tushar Pramanik <tushar.pramanik@lastingdynamics.com>
2024-02-08 18:38:34 +0600

    player instantiation over the network

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -15,6 +15,9 @@ namespace Com.TusharPramanikBD.MyGame
 
         public static GameManager Instance;
 
+        [Tooltip("The prefab to use for representing the player")]
+        public GameObject playerPrefab;
+
         #endregion
 
         #region MonoBehaviour CallBacks
@@ -22,8 +25,26 @@ namespace Com.TusharPramanikBD.MyGame
         void Start()
         {
             Instance = this;
+
+            if (playerPrefab == null)
+            {
+                Debug.LogError("<Color=Red><a>Missing</a></Color> playerPrefab Reference. Please set it up in GameObject 'Game Manager'",this);
+            }
+            else
+            {
+                if (PlayerManager.LocalPlayerInstance == null)
+                {
+                    Debug.LogFormat("We are Instantiating LocalPlayer from {0}", SceneManager.GetActiveScene().name);
+                    // we're in a room. spawn a character for the local player. it gets synced by using PhotonNetwork.Instantiate
+                    PhotonNetwork.Instantiate(this.playerPrefab.name, new Vector3(0f, 5f, 0f), Quaternion.identity, 0);
+                }
+                else
+                {
+                    Debug.LogFormat("Ignoring scene load for {0}", SceneManager.GetActiveScene().name);
+                }
+            }
         }
-        
+
         #endregion
 
         #region Photon Callbacks
--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -19,6 +19,9 @@ namespace Com.TusharPramanikBD.MyGame
         [Tooltip("The current Health of our player")]
         public float Health = 1f;
 
+        [Tooltip("The local player instance. Use this to know if the local player is represented in the Scene")]
+        public static GameObject LocalPlayerInstance;
+
         #endregion
 
         #region Private Fields
@@ -36,6 +39,17 @@ namespace Com.TusharPramanikBD.MyGame
 
         void Awake()
         {
+            // #Important
+            // used in GameManager.cs: we keep track of the localPlayer instance to prevent instantiation when levels are synchronized
+            if (photonView.IsMine)
+            {
+                PlayerManager.LocalPlayerInstance = this.gameObject;
+            }
+
+            // #Critical
+            // we flag as don't destroy on load so that instance survives level synchronization, thus giving a seamless experience when levels load.
+            DontDestroyOnLoad(this.gameObject);
+
             if (beams == null)
             {
                 Debug.LogError("<Color=Red><a>Missing</a></Color> Beams Reference.", this);
@@ -64,6 +78,11 @@ namespace Com.TusharPramanikBD.MyGame
             {
                 Debug.LogError("<Color=Red><a>Missing</a></Color> CameraWork Component on playerPrefab.", this);
             }
+
+            #if UNITY_5_4_OR_NEWER
+            // Unity 5.4 has a new scene management. register a method to call CalledOnLevelWasLoaded.
+            UnityEngine.SceneManagement.SceneManager.sceneLoaded += OnSceneLoaded;
+            #endif
         }
 
         void Update()
@@ -132,6 +151,32 @@ namespace Com.TusharPramanikBD.MyGame
             Health -= 0.1f * Time.deltaTime;
         }
 
+        #if !UNITY_5_4_OR_NEWER
+        /// <summary>See CalledOnLevelWasLoaded. Outdated in Unity 5.4.</summary>
+        void OnLevelWasLoaded(int level)
+        {
+            this.CalledOnLevelWasLoaded(level);
+        }
+        #endif
+
+        void CalledOnLevelWasLoaded(int level)
+        {
+            // check if we are outside the Arena and if it's the case, spawn around the center of the arena in a safe zone
+            if (!Physics.Raycast(transform.position, -Vector3.up, 5f))
+            {
+                transform.position = new Vector3(0f, 5f, 0f);
+            }
+        }
+
+        #if UNITY_5_4_OR_NEWER
+        public override void OnDisable()
+        {
+            // Always call the base to remove callbacks
+            base.OnDisable ();
+            UnityEngine.SceneManagement.SceneManager.sceneLoaded -= OnSceneLoaded;
+        }
+        #endif
+
         #endregion
 
         #region Custom
@@ -139,7 +184,7 @@ namespace Com.TusharPramanikBD.MyGame
         /// <summary>
         /// Processes the inputs. Maintain a flag representing when the user is pressing Fire.
         /// </summary>
-        void ProcessInputs()
+        private void ProcessInputs()
         {
             if (Input.GetButtonDown("Fire1"))
             {
@@ -156,6 +201,13 @@ namespace Com.TusharPramanikBD.MyGame
                 }
             }
         }
+
+        #if UNITY_5_4_OR_NEWER
+        void OnSceneLoaded(UnityEngine.SceneManagement.Scene scene, UnityEngine.SceneManagement.LoadSceneMode loadingMode)
+        {
+            this.CalledOnLevelWasLoaded(scene.buildIndex);
+        }
+        #endif
 
         #endregion
 
